4e16ce5c0d8a861543f44a82bad876dc92d370a7
Marc Cane <marc00077@hotmail.com>
2024-04-04 22:28:52 +0200

    Add printf functionality. Add song with variable length tones. Still doesn't sound right

--- buzzer.ino
+++ buzzer.ino
@@ -1,39 +1,128 @@
 #define BUZZER_PIN 11 // The Arduino pin connected to the buzzer
 #define LED_PIN 2
 
-const PROGMEM float melody2[] = {
-  587.3,370,493.9,370,587.3,370,493.9,370,587.3,370,493.9,370,587.3,370,493.9,370,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,784,493.9,659.3,493.9,784,493.9,659.3,493.9,784,493.9,659.3,493.9,784,493.9,659.3,493.9,659.3,440,554.4,440,659.3,440,554.4,440,659.3,440,554.4,440,659.3,440,554.4,440,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,587.3,370,493.9,370,587.3,370,493.9,370,587.3,370,493.9,370,587.3,370,493.9,370,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,784,493.9,659.3,493.9,784,493.9,659.3,493.9,784,493.9,659.3,493.9,784,493.9,659.3,493.9,659.3,440,554.4,440,659.3,440,554.4,440,659.3,440,554.4,440,659.3,440,554.4,440,587.3,370,493.9,370,587.3,370,493.9,370,587.3,370,493.9,370,587.3,370,493.9,370,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,587.3,415.3,493.9,415.3,784,493.9,659.3,493.9,784,493.9,659.3,493.9,784,493.9,659.3,493.9,784,493.9,659.3,493.9,659.3,440,554.4,440,659.3,440,554.4,440,659.3,440,554.4,440,659.3,440,554.4,440,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,740,987.8,740,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1174.7,830.6,987.8,830.6,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1568,987.8,1318.5,987.8,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7,880,1318.5,880,1108.7
+FILE f_out;
+int sput(char c, __attribute__((unused)) FILE* f) {return !Serial.write(c);}
+
+const PROGMEM float melodyFreq[] = {
+  330,1,494,1,660,1,590,660,494,494,523,1,440,1,494,1,392,392,440,370,1,392,1,330,330,370,1,294,1,330,247,247,261,1,311,1,330,1,247,247,262,1,370,1,330,1,494,1,660,1,590,660,494,494,523,1,440,1,494,1,392,392,440,370,1,392,1,330,330,370,1,294,1,330,247,247,261,1,311,1,330,1,247,247,262,1,370,1,330
+};
+
+const PROGMEM unsigned melodyLengths[] = {
+  150,40,159,40,150,40,150,150,100,100,150,40,150,40,150,40,100,100,150,150,40,150,40,100,100,150,40,150,40,150,100,100,150,40,150,40,150,40,100,100,150,40,150,40,150,40,159,40,150,40,150,150,100,100,150,40,150,40,150,40,100,100,150,150,40,150,40,100,100,150,40,150,40,150,100,100,150,40,150,40,150,40,100,100,150,40,150,40,150,40
 };
 
 void setup() {
+  Serial.begin(115200);
+  fdev_setup_stream(&f_out, sput, nullptr, _FDEV_SETUP_WRITE); // cf https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gaf41f158c022cbb6203ccd87d27301226
+  stdout = &f_out;
+
+  printf("sizeof unsigned = %d", sizeof(unsigned));
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+  // digitalWrite(LED_PIN, 1);
+  // noTone(BUZZER_PIN);
+  // tone(BUZZER_PIN, 800, 1000); //Error beep
+  // delay(1000);
 
   // iterate over the notes of the melody:
-  int size = sizeof(melody2) / sizeof(float);
+  int size = sizeof(melodyFreq) / sizeof(float);
+  int size2 = sizeof(melodyLengths) / sizeof(unsigned);
 
-  for (int thisNote = 0; thisNote < size; thisNote++) {
+  if(size+1 != size2){
+    printf("Problemo de lengths! %d %d\n", size, size2);
+    tone(BUZZER_PIN, 500, 1000); //Error beep
+  }
+  else
+  {
+    for (int thisNote = 0; thisNote < size; thisNote++) {
 
-    // to calculate the note duration, take one second divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    // int noteDuration = 1000 / noteDurations[thisNote];
-    unsigned noteFreq = pgm_read_float_near(melody2 + thisNote);
-    //(unsigned)melody2[thisNote]
-    tone(BUZZER_PIN, noteFreq, 122);
+      // to calculate the note duration, take one second divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      // int noteDuration = 1000 / noteDurations[thisNote];
+      unsigned noteFreq = pgm_read_float_near(melodyFreq + thisNote);
+      //(unsigned)melodyFreq[thisNote]
 
-    digitalWrite(LED_PIN, noteFreq > 1000 ? 1 : 0);
-    digitalWrite(LED_BUILTIN, noteFreq > 900 ? 1 : 0);
+      unsigned long noteLen = pgm_read_word_near(melodyLengths + thisNote);
+      // unsigned long noteLen = 100;
+      tone(BUZZER_PIN, noteFreq, noteLen);
 
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    // int pauseBetweenNotes = 122 * 1.30;
-    // delay(pauseBetweenNotes);
-    delay(122);
-    // stop the tone playing:
-    // noTone(BUZZER_PIN);
+      digitalWrite(LED_PIN, noteFreq > 1000 ? 1 : 0);
+      digitalWrite(LED_BUILTIN, noteFreq > 900 ? 1 : 0);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      // int pauseBetweenNotes = 122 * 1.30;
+      // delay(pauseBetweenNotes);
+      delay(noteLen);
+      // stop the tone playing:
+      // noTone(BUZZER_PIN);
+    }
   }
 }
 
+int i = 0;
+bool blink = false;
 void loop() {
-  // no need to repeat the melody.
+  blink = !blink;
+  digitalWrite(LED_PIN, blink);
+  printf("Loop %d\n", i++);
+  delay(200);
 }
+
+
+// #define BUZZER_PIN 11 // The Arduino pin connected to the buzzer
+// #define LED_PIN 2
+
+// const PROGMEM float melodyFreq[] = {
+// 330,1,494,1,660,1,590,660,494,494,523,1,440,1,494,1,392,392,440,370,1,392,1,330,330,370,1,294,1,330,247,247,261,1,311,1,330,1,247,247,262,1,370,1,330,1,494,1,660,1,590,660,494,494,523,1,440,1,494,1,392,392,440,370,1,392,1,330,330,370,1,294,1,330,247,247,261,1,311,1,330,1,247,247,262,1,370,1,330
+// };
+
+// const PROGMEM unsigned melodyLengths[] = {
+// 150,40,159,40,150,40,150,150,100,100,150,40,150,40,150,40,100,100,150,150,40,150,40,100,100,150,40,150,40,150,100,100,150,40,150,40,150,40,100,100,150,40,150,40,150,40,159,40,150,40,150,150,100,100,150,40,150,40,150,40,100,100,150,150,40,150,40,100,100,150,40,150,40,150,100,100,150,40,150,40,150,40,100,100,150,40,150,40,150,40
+// };
+
+// void setup() {
+//   Serial.print("TEEEST");
+//   printf("Test\n");
+//   pinMode(LED_BUILTIN, OUTPUT);
+//   pinMode(LED_PIN, OUTPUT);
+
+//   // iterate over the notes of the melody:
+//   int size = sizeof(melodyFreq) / sizeof(float);
+//   int size2 = sizeof(melodyLengths) / sizeof(float);
+
+//   if(size != size2){
+//     printf("Problemo de lengths! %d %d\n", size, size2);
+//     tone(BUZZER_PIN, 500, 0); //Error beep
+//     while(1);
+//   }
+
+//   for (int thisNote = 0; thisNote < size; thisNote++) {
+
+//     // to calculate the note duration, take one second divided by the note type.
+//     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+//     // int noteDuration = 1000 / noteDurations[thisNote];
+//     unsigned noteFreq = pgm_read_float_near(melodyFreq + thisNote);
+//     //(unsigned)melodyFreq[thisNote]
+
+//     // unsigned long noteLen = pgm_read_float_near(melodyLengths + thisNote);
+//     unsigned long noteLen = 100;
+//     tone(BUZZER_PIN, noteFreq, noteLen);
+
+//     digitalWrite(LED_PIN, noteFreq > 1000 ? 1 : 0);
+//     digitalWrite(LED_BUILTIN, noteFreq > 900 ? 1 : 0);
+
+//     // to distinguish the notes, set a minimum time between them.
+//     // the note's duration + 30% seems to work well:
+//     // int pauseBetweenNotes = 122 * 1.30;
+//     // delay(pauseBetweenNotes);
+//     delay(noteLen);
+//     // stop the tone playing:
+//     // noTone(BUZZER_PIN);
+//   }
+// }
+
+// void loop() {
+//   // no need to repeat the melody.
+// }
